06e036183481b04d2cd260b6b9fb166c08623592
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2025-11-17 20:24:11 +0530

    Refactor About component layout and image attributes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="w-full h-screen grid grid-cols-1 md:grid-cols-2 ">
-
+    <div className="w-full h-screen grid grid-cols-1 md:grid-cols-2">
+      
       {/* Left Column */}
       <div className="bg-indigo-500 flex justify-center items-center text-white font-bold">
-        <img src='https://i.pinimg.com/1200x/bd/f0/ef/bdf0ef6f416e52d00d21ca488f9530e9.jpg' />
+        <img 
+          src="https://i.pinimg.com/1200x/bd/f0/ef/bdf0ef6f416e52d00d21ca488f9530e9.jpg" 
+          alt="Profile"
+          className="h-full w-full object-cover"
+        />
       </div>
 
-      {/* Right Column (4 rows with different heights) */}
-      <div className="flex flex-col h-full">
-        <P>ABOUT ME</P>
+      {/* Right Column */}
+      <div className="flex flex-col h-full justify-center items-center">
+        <p className="text-6xl font-extrabold">ABOUT ME</p>
       </div>
-
     </div>
   );
 };
