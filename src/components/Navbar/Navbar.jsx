75ae9aa09973ff0041a71fbca3ae4ce633f9e57a
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2025-11-18 10:35:47 +0530

    Fix socialLinks entry for Instagram in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
  const socialLinks = [
    { name: 'GitHub', icon: <GitHubIcon />, href: 'https://github.com/adityaPawar1999' },
    { name: 'LinkedIn', icon: <LinkedInIcon />, href: 'https://www.linkedin.com/in/aditya-pawar-857247216' },
-   { name: 'LinkedIn', icon: <InstagramIcon />, href: 'https://www.instagram.com/adityapawa.r/?igsh=MXFjc3ppZW44eHh5Yw%3D%3D' },
+   { name: 'Instagram', icon: <InstagramIcon />, href: 'https://www.instagram.com/adityapawa.r/?igsh=MXFjc3ppZW44eHh5Yw%3D%3D' },
   
 
 
