d7377010b76650df3bdaf4f41f3288596d3c4aef
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2025-11-18 20:05:54 +0530

    Update import path for SelfPic image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import PlaceIcon from '@mui/icons-material/Place';
 import BusinessIcon from '@mui/icons-material/Business';
 import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
 import AssessmentIcon from '@mui/icons-material/Assessment';
-import SelfPic from './Screenshot_20251118_195452_Instagram';
+import SelfPic from 'src/pages/Screenshot_20251118_195452_Instagram.jpg';
 
 
 export default function About() {
