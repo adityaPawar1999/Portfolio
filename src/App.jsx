aa3cb136e1b6baded4b756a2419c238601f8803d
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2026-02-23 11:41:54 +0530

    Refactor routes in App component for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Account from './Modules/Accounting/Account'
-
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Account from "./Modules/Accounting/Account";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          {/* Home */}
           <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Account" element={<Account />} />
-          <Route path="/crm" element={<NotFound />} />
-          <Route path="/sales" element={<NotFound />} />
-          <Route path="/stock" element={<NotFound />} />
 
-           <Route path="*" element={<NotFound />} />
-          
+          {/* Other Pages */}
+          <Route path="about" element={<About />} />
+          <Route path="account" element={<Account />} />
+          <Route path="crm" element={<NotFound />} />
+          <Route path="sales" element={<NotFound />} />
+          <Route path="stock" element={<NotFound />} />
+
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
